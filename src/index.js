2b1ea8751950cfe4ced749bfe73f14d88eb0391a
ArtemTeslenko <artem.teslenko.w126@gmail.com>
2022-07-28 12:12:43 +0300

    Fix js for different buttons

--- src/index.js
+++ src/index.js
@@ -31,9 +31,15 @@ const splide = new Splide('.splide', {
     menu: document.querySelector('.backdrop-mb-menu'),
     body: document.querySelector('body'),
     backdrop: document.querySelector('.backdrop'),
-    cardBtn: document.querySelector('.products__btn-common'),
-    frontEl: document.querySelector('.front'),
-    backEl: document.querySelector('.back'),
+    cardBtn1: document.querySelector('.products__btn-common1'),
+    frontEl1: document.querySelector('.front1'),
+    backEl1: document.querySelector('.back1'),
+    cardBtn2: document.querySelector('.products__btn-common2'),
+    frontEl2: document.querySelector('.front2'),
+    backEl2: document.querySelector('.back2'),
+    cardBtn3: document.querySelector('.products__btn-common3'),
+    frontEl3: document.querySelector('.front3'),
+    backEl3: document.querySelector('.back3'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -41,7 +47,9 @@ const splide = new Splide('.splide', {
   refs.openModalBtn.addEventListener('click', addClassModal);
   refs.openModalBtnNMenu.addEventListener('click', addClassModal);
   refs.closeModalBtn.addEventListener('click', removeClassModal);
-  refs.cardBtn.addEventListener('click', toggleCardClass);
+  refs.cardBtn1.addEventListener('click', toggleCardClass1);
+  refs.cardBtn2.addEventListener('click', toggleCardClass2);
+  refs.cardBtn3.addEventListener('click', toggleCardClass3);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
@@ -58,8 +66,16 @@ const splide = new Splide('.splide', {
     refs.body.classList.remove('no-scroll');
   }
 
-  function toggleCardClass() {
-    refs.frontEl.classList.toggle('is-passive');
-    refs.backEl.classList.toggle('is-active');
+  function toggleCardClass1() {
+    refs.frontEl1.classList.toggle('is-passive');
+    refs.backEl1.classList.toggle('is-active');
+  }
+  function toggleCardClass2() {
+    refs.frontEl2.classList.toggle('is-passive');
+    refs.backEl2.classList.toggle('is-active');
+  }
+  function toggleCardClass3() {
+    refs.frontEl3.classList.toggle('is-passive');
+    refs.backEl3.classList.toggle('is-active');
   }
 })();
